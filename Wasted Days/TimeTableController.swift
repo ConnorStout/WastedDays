f4bc39274ef0c6b538b78ec3884ec6a07e16d3a7
Connor Stout <cstout179@gmail.com>
2016-12-29 14:41:50 -0500

    added sleep chart and fixed core data issue

--- Wasted Days/TimeTableController.swift
+++ Wasted Days/TimeTableController.swift
@@ -14,6 +14,7 @@ class TimeTableController: UITableViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     
