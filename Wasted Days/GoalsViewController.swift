686d102a51dfa98abcaeecb18a5ab3766aab7b8e
Connor Stout <cstout179@gmail.com>
2016-12-31 02:54:02 -0500

    final prep for store submission

---
+++ Wasted Days/GoalsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GoalsViewController.swift
+//  Wasted Days
+//
+//  Created by Connor Stout on 12/29/16.
+//  Copyright © 2016 Connor Stout. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class GoalsViewController: UIViewController{
+  
+    override func viewDidLoad(){
+        
+    }
+}
